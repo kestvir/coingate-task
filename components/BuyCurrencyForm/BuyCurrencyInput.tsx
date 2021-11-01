a9b40cbbb77c128a1582e4901064f0d735e0d705
kestutisvirbickas <v.kestutis123@gmail.com>
2021-11-01 19:18:07 +0200

    Change project structure

---
+++ components/BuyCurrencyForm/BuyCurrencyInput.tsx
@@ -0,0 +1,74 @@
+import { SyntheticEvent } from "react";
+import { styled } from "@mui/material/styles";
+import InputAdornment from "@mui/material/InputAdornment";
+import { Divider, InputBase, Box } from "@mui/material";
+
+import BuyCurrencyAutocomplete from "./BuyCurrencyAutocomplete";
+import CurrencyImage from "../UI/CurrencyImage";
+
+const StyledInputWrapper = styled(Box)({
+  display: "flex",
+  alignItems: "center",
+  border: "1px solid #D9D9D9",
+  borderRadius: "20px",
+  maxHeight: "48px",
+});
+
+const StyledInputField = styled(InputBase)({
+  width: "70%",
+  paddingLeft: "20px",
+});
+
+const StyledDivider = styled(Divider)({
+  height: "28px",
+  margin: "4px",
+  color: "#D9D9D9",
+});
+
+type Props = {
+  value: string;
+  id: string;
+  startAdormentText: string;
+  currency: string;
+  currencies: Array<string>;
+  handleChangeCurrency: (
+    event: SyntheticEvent<Element, Event>,
+    value: any
+  ) => void;
+};
+
+export default function BuyCurrencyInput({
+  value,
+  id,
+  startAdormentText,
+  currency,
+  currencies,
+  handleChangeCurrency,
+}: Props) {
+  return (
+    <StyledInputWrapper>
+      <StyledInputField
+        value={value}
+        inputProps={{ style: { textAlign: "center" } }}
+        startAdornment={
+          <InputAdornment position="start">{startAdormentText}</InputAdornment>
+        }
+      />
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="space-around"
+        width={"30%"}
+      >
+        <StyledDivider orientation="vertical" />
+        {currency && <CurrencyImage currency={currency} />}
+        <BuyCurrencyAutocomplete
+          id={id}
+          value={currency}
+          currencies={currencies}
+          handleChangeCurrency={handleChangeCurrency}
+        />
+      </Box>
+    </StyledInputWrapper>
+  );
+}
---
+++ components/UI/StyledButton.tsx
@@ -0,0 +1,36 @@
+import { ReactNode } from "react";
+
+import { styled } from "@mui/material/styles";
+import Button, { ButtonProps as MuiButtonProps } from "@mui/material/Button";
+
+type Props = MuiButtonProps & {
+  children: ReactNode;
+  icon?: ReactNode;
+};
+
+const StyledEmotionButton = styled(Button)(({ theme }) => ({
+  background: theme.palette.primary.main,
+  color: "#fff",
+  fontSize: "1.2em",
+  fontWeight: 500,
+  minWidth: "145px",
+  borderRadius: "4px 50px 50px 50px",
+  textTransform: "none",
+  height: "50px",
+  "&:hover": {
+    background: theme.palette.primary.main,
+    boxShadow: `0 3px 11px ${theme.palette.primary.main}`,
+  },
+  "&:disabled": {
+    color: "#fff",
+    background: "#B5B5B5",
+  },
+}));
+
+export default function StyledButton({ children, icon, ...rest }: Props) {
+  return (
+    <StyledEmotionButton {...rest} endIcon={icon}>
+      {children}
+    </StyledEmotionButton>
+  );
+}
